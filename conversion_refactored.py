d03aaa82036ec24636c74999c7e3d9a9ef699d48
KcirtapdiaZ <patrick.m.diaz@gmail.com>
2025-03-08 19:25:01 +0000

    Update conversion_refactored.py

--- conversion_refactored.py
+++ conversion_refactored.py
@@ -2,17 +2,34 @@ class ConversionNotPossibleException(Exception):
     pass
 
 def convert(fromUnit: str, toUnit: str, value: float) -> float:
-    conversions = {
-        "Celsius": {"Kelvin": value + 273.15, "Fahrenheit": (value * 9/5) + 32},
-        "Fahrenheit": {"Celsius": (value - 32) * 5/9, "Kelvin": (value - 32) * 5/9 + 273.15},
-        "Kelvin": {"Celsius": value - 273.15, "Fahrenheit": (value - 273.15) * 9/5 + 32},
-        # Add distance conversions here...
+    conversion_map = {
+        'Celsius': {
+            'Kelvin': lambda celsius: celsius + 273.15,
+            'Fahrenheit': lambda celsius: celsius * 9 / 5 + 32
+        },
+        'Fahrenheit': {
+            'Celsius': lambda fahrenheit: (fahrenheit - 32) * 5 / 9,
+            'Kelvin': lambda fahrenheit: (fahrenheit + 459.67) * 5 / 9
+        },
+        'Kelvin': {
+            'Celsius': lambda kelvin: kelvin - 273.15,
+            'Fahrenheit': lambda kelvin: (kelvin * 9 / 5) - 459.67
+        },
+        'Miles': {
+            'Yards': lambda miles: miles * 1760,
+            'Meters': lambda miles: miles * 1609.34
+        },
+        'Yards': {
+            'Miles': lambda yards: yards / 1760,
+            'Meters': lambda yards: yards * 0.9144
+        },
+        'Meters': {
+            'Miles': lambda meters: meters / 1609.34,
+            'Yards': lambda meters: meters / 0.9144
+        }
     }
     
-    if fromUnit == toUnit:
-        return value
-    
     try:
-        return conversions[fromUnit][toUnit]
+        return conversion_map[fromUnit][toUnit](value)
     except KeyError:
         raise ConversionNotPossibleException(f"Conversion from {fromUnit} to {toUnit} is not possible.")
